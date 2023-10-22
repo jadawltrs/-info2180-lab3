a381fae25ad0cf0d2d8929920d96aa98717def9c
Jada Walters <jada.walters@mymona.uwi.edu>
2023-10-22 16:53:32 -0500

    added event listener to add x's and o's

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,8 +3,34 @@ document.addEventListener("DOMContentLoaded", function() {
     boardDiv=document.getElementById("board")
     squareDivs=boardDiv.querySelectorAll("div")
 
+    
     // for each div set it as a square
     squareDivs.forEach(squareDivs =>{
         squareDivs.setAttribute("class", "square")
+    })
+    
+    // game state for keeping track of plays
+    state=["","","","","","","","",""]
+    let X=true    
+    
+    sqs=boardDiv.querySelectorAll(".square");
+    sqs.forEach((sq,index) =>{
+        sq.addEventListener("click",()=>{
+            if (state[index]===""){
+                if (X){
+                    sq.textContent="X"
+                    sq.classList.add("square.X")
+                    state[index]="X"
+                    X=!X
+                }
+                else{
+                    sq.textContent="O"
+                    sq.classList.add("square.O")
+                    state[index]="O"
+                    X=!X
+                }
+            }
+
+        })
     })
 });
